9cda3c62b8e3bd1ab8062322f4294fa72254c6cb
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-02 17:52:21 -0800

    Add files via upload

--- assertEqual.js
+++ assertEqual.js
@@ -7,15 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-function head(array) {
-  return array[0];
-}
-
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-console.log(head([1]));
-console.log(head([]));
--- tail.js
+++ tail.js
@@ -1,22 +1,27 @@
-/**
- * Gets all but the first element of `array`.
- *
- * @since 4.0.0
- * @category Array
- * @param {Array} array The array to query.
- * @returns {Array} Returns the slice of `array`.
- * @example
- *
- * tail([1, 2, 3])
- * // => [2, 3]
- */
-function tail(array) {
-  const length = array == null ? 0 : array.length
-  if (!length) {
-    return []
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  const [, ...result] = array
-  return result
-}
+};
 
-export default tail
+const tail = function(input) {
+  let tailArray = [];
+  for (let i = 1; i < input.length; i++) {
+    tailArray.push(input[i]);
+  }
+  return tailArray;
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail([1]));
+console.log(tail([]));
