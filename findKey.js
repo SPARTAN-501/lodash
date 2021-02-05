424c2d59001ede5c41fb42ba5f275bbdb04fe8e8
Luke Yin <77079264+SPARTAN-501@users.noreply.github.com>
2021-02-04 19:02:45 -0800

    Add files via upload

--- findKey.js
+++ findKey.js
@@ -1,38 +1,27 @@
-/**
- * This method is like `find` except that it returns the key of the first
- * element `predicate` returns truthy for instead of the element itself.
- *
- * @since 1.1.0
- * @category Object
- * @param {Object} object The object to inspect.
- * @param {Function} predicate The function invoked per iteration.
- * @returns {string|undefined} Returns the key of the matched element,
- *  else `undefined`.
- * @see find, findIndex, findLast, findLastIndex, findLastKey
- * @example
- *
- * const users = {
- *   'barney':  { 'age': 36, 'active': true },
- *   'fred':    { 'age': 40, 'active': false },
- *   'pebbles': { 'age': 1,  'active': true }
- * }
- *
- * findKey(users, ({ age }) => age < 40)
- * // => 'barney' (iteration order is not guaranteed)
- */
-function findKey(object, predicate) {
-  let result
-  if (object == null) {
-    return result
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-  Object.keys(object).some((key) => {
-    const value = object[key]
-    if (predicate(value, key, object)) {
-      result = key
-      return true
+};
+
+const findKey = function(object, callback) {
+  let result = undefined;
+  const keys = Object.keys(object);
+  for (let i = 0; i < keys.length; i++) {
+    if (callback(object[keys[i]]) === true) {
+      result = keys[i];
+      return result;
     }
-  })
-  return result
+  }
 }
 
-export default findKey
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
